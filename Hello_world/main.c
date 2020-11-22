815841c918b73cfdc9dcf2b2d7fe3f88faaa5b0a
cmousikos <74675716+cmousikos@users.noreply.github.com>
2020-11-22 22:08:22 +0200

    Add files via upload

---
+++ Hello_world/main.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: mousikos
+ *
+ * Created on November 22, 2020, 6:42 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char* argv[])
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
